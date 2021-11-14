056684395532c84db737a410a11ab3e24d46deb4
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-11-14 10:53:34 -0800

    intitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Video } from './Video';
+import { Menu } from './Menu';
+// App passes down this.state.src to Video. Video uses this info to display the chosen video. App also passes down this.state.src to Menu and menu uses this ability to let a user select a new video. 
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chooseVideo = this.chooseVideo.bind(this);
+
+    this.state = { src: VIDEOS.fast };
+  }
+   chooseVideo(newVideo) {
+      this.setState({
+        src: VIDEOS[newVideo]
+      });
+   }
+
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1> 
+        <Menu chooseVideo={this.chooseVideo} />
+        <Video  src={this.state.src} />
+      </div>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(
+  <App /> , 
+  document.getElementById('app')
+);
